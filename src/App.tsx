4d34f97f580e5782f5f8f4f8b5f283b57e3d8c39
kalz2q <katsu95i@gmail.com>
2022-09-16 04:04:56 +0900

    m

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,11 @@ function App() {
   return (
     <div className="App">
       <div className="triangle"></div>
+      <div className="triangle2">
+        {/* <div className="trinagle3"></div> */}
+      </div>
+      <div className="triangle"></div>
+      <div className="triangle3"></div>
     </div>
   );
 }
